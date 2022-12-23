050c837d770a03cb172487d06405263c9d6014c8
Joshua <cutejosh2@gmail.com>
2022-12-23 12:47:49 +0100

    fixed navigation and routes bug

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
-
+import { Routes as Switch, Route, Navigate } from 'react-router-dom'
+import { Results } from './Results'
 export const Routes = () => {
   return (
-	<div>Routes</div>
+    <div className='p-4'>
+      <Switch>
+        <Route path="/" element={<Navigate replace to="/search" />} />
+        <Route exact path='/search' element={<Results />} />
+        <Route exact path='/images' element={<Results />} />
+        <Route exact path='/news' element={<Results />} />
+        <Route exact path='/videos' element={<Results />} />
+      </Switch>
+    </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from "react-router-dom";
 import App from './App';
 import './global.css'
 
-ReactDOM.render(
-	<Router>
-		<App />
-	</Router>
-	, document.getElementById('root'))
+const root = createRoot(document.getElementById('root'));
+
+root.render(<Router><App /></Router>)
